8bedcfa963d763cd23ca9cfa10804efbebd0ddb2
Marcelo Gonzalez <mdgonzalez470@gmail.com>
2022-06-23 14:05:38 +0200

    proyectoFinalv2

--- JS/app06_02.js
+++ JS/app06_02.js
@@ -18,19 +18,14 @@ padreButton.addEventListener("click",doSomething);
 
 
 function doSomething(j){
-
     let buttonGroup=document.getElementById('secondButtonGroup');
     let clickedItem="";
-
     (j.target != j.currentTarget) ? clickedItem=j.target.id : j.stopPropagation();//Operador Ternario
-    
     {
-         
          if(clickedItem=="categoryTerapia"){
             buttonGroup.innerHTML = ""; 
             addButtonsGroup(area1,"secondButtonGroup");
          }
-
         else if (clickedItem=="diagnosticoTerapia"){
             buttonGroup.innerHTML = "";
             addButtonsGroup(area2,"secondButtonGroup");
@@ -39,10 +34,7 @@ function doSomething(j){
             buttonGroup.innerHTML = "";
             addButtonsGroup(area3,"secondButtonGroup");
         }
-
     }
-   
-
 }
 
 let padreButtonServicio=document.getElementById('secondButtonGroup');
@@ -105,35 +97,30 @@ const addButtonsGroup=(x,y) =>{
     console.log(buttonGroup);
     buttonGroup.innerHTML+=`
     <div class="btn-group" id="secondButtonGroup" role="group" aria-label="Basic radio toggle button group">
-    <label for="" class="col-md-2 col-form-label "><h3>Servicio</h3></label>
-    
-    <div class="buttonServicios">
-     
-    </div>
-  </div> 
+        <label for="" class="col-md-2 col-form-label "><h3>Servicio</h3></label>
+        <div class="buttonServicios">
+        </div>
+     </div> 
     
     `;
-
     buttonServicio=document.querySelector(`.buttonServicios`);
-    
     for (let servicio of x) {
-   
     buttonServicio.innerHTML+=`
-    
       <button class="btn btn-primary btn-lg rounded-pill" type="button" id=${servicio}>${servicio}</button>
-    
     `;
-    
 }
 }
-
-
-
 //Creacion de array de Servicios
 const area1=["Terapia Individual","terapia de Pereja","terapia de Niños"];
 const area2=["Asesoramiento Clinico","Orientacion a Padres"];
 const area3=["Evaluacion Psicologica","Psicodiagnostico"];
 
+
+let arrayDataJson=[];
+let arrayTerapiaIndividual=[];
+let arrayTerapiaPareja=[];
+let arrayTerapiaNiños=[];
+let arrayAsoramientoClinico=[];
 
 
 const areas=[area1,area2,area3];
@@ -148,65 +135,95 @@ const guardar=() =>{
         timer: 1500
       })
 }
-
-let arrayDataJson=[];
-let arrayTerapiaIndividual=[];
-let arrayTerapiaPareja=[];
-let arrayTerapiaNiños=[];
-let arrayAsoramientoClinico=[];
-
-const añadirProfesional=(x) =>{
-    let buttonGroup="";
+let buttonGroup="";
+const añadirProfesional=() =>{
     buttonGroup=document.getElementById("thirdButtonGroup");
-    
     buttonGroup.innerHTML ="";  // limpio pantalla
-   
-    arrayTerapiaIndividual.forEach(element => {
-       
+    renderThirdButton();
+    renderProfesional();
+}
+const cleaveFunctionDelay =()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(cleaveFunction());
+        },3000);
+    });
+}
+const renderThirdButton=() =>{
+    arrayTerapiaIndividual.forEach(e=> {
         buttonGroup.innerHTML+=`
         <div class="caja" style="text-align=center;">       
-            
-            <img style="width:200px"; height="200" src="${element.imagen}" class="rounded-circle" alt="...">
-       
-        
-            <!-- <p>${element.matricula}</p> -->
+            <img style="width:200px"; height="200" src="${e.imagen}" class="rounded-circle" alt="...">
+            <!-- <p>${e.matricula}</p> -->
             </br>
             </br>
-            <button class="btn btn-primary btn-lg rounded-pill" type="button" id="btn${element.id}"> <p>${element.profesional}</p> </button>
+            <button class="btn btn-primary btn-lg rounded-pill" type="button" id="btn${e.id}"> <p>${e.profesional}</p> </button>
             <div class="parrafo"></div>    
             </br>
             </br>
         </div>`
         });
-
-    arrayTerapiaIndividual.forEach(boton => {
-        
-        document.querySelector(`#btn${boton.id}`).addEventListener("click",()=>{
+}
+const renderProfesional=() =>{
+    arrayTerapiaIndividual.forEach(e => {       
+        document.querySelector(`#btn${e.id}`).addEventListener("click",()=>{
             buttonGroup.innerHTML ="";
-            
             buttonGroup.innerHTML=buttonGroup.innerHTML=`
-           <div class="caja" style="text-align=center;">       
-               
-               <img style="width:200px"; height="200" src="${boton.imagen}" class="rounded-circle" alt="...">
-          
-           
-               <p>${boton.profesional}
-               <p>${boton.descripcion}
+            <div class="caja" style="text-align=center;">       
+               <img style="width:200px"; height="200" src="${e.imagen}" class="rounded-circle" alt="...">
+               <p>${e.profesional}
+               <p>${e.descripcion}
                </br>
                </br>
-               
                <div class="parrafo"></div>    
+               <div class="row mb-3">
+               <label id="birthGroup" for="dob" class="col-sm-3 col-form-label text-sm-right">Seleccione la fecha</label>
+                   <div class="col">
+                       <input id="birth" name="dob" type="date" class="col-6 form-control" aria-describedby="dob-feedback">
+                        <div id="dob-feedback" class="invalid-feedback">
+                        </div>
+                   </div>
+               </div>
                </br>
                </br>
-           </div>`
+            </div>`
         });
-        
-    })     
+    });
 }
 
-fetch("../datos.json")
-    .then(resp=>resp.json())
-    .then(data=>{arrayDataJson=data;
-    });
-  
 
+fetch("../datos.json").then(resp=>resp.json()).then(data=>{arrayDataJson=data;});
+  
+  const cleaveFunction=() =>{
+    new Cleave('.input-element', {
+        creditCard: true,
+        delimiter: '-',
+        onCreditCardTypeChanged: function (type) {
+            console.log(type)
+            if(type === 'visa'){
+                document.querySelector('.fa-cc-visa').classList.add('active');
+            } else {
+                document.querySelector('.fa-cc-visa').classList.remove('active');
+            }
+        }
+    });
+   }
+    
+// agregado 23/06
+   let popup=document.getElementById("popup");
+   let closebtn=document.getElementById("closebtn");
+   let singupbtn=document.getElementById("signUpBtn");
+   
+   singupbtn.onclick=function(){
+       popup.style.display="block";
+       cleaveFunctionDelay();
+   
+   }
+   closebtn.onclick=function(){
+       popup.style.display="none";   
+   }
+   window.onclick=function(event){
+       if(event.target==popup){
+           popup.style.display="none";
+       }
+   }v
